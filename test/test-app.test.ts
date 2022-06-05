518705fee971871888972f410e26e7a9141c3712
junkor-1011 <tomoyukizxy@gmail.com>
2022-06-05 23:51:16 +0900

    feat: add fine grained assertions

--- test/test-app.test.ts
+++ test/test-app.test.ts
@@ -1,15 +1,19 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as TestApp from '../lib/test-app-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as TestApp from '../lib/test-app-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/test-app-stack.ts
-test('SQS Queue Created', () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new TestApp.TestAppStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
+test('Lambda Function: Hello Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TestApp.TestAppStack(app, 'MyTestStack', {
+    projectName: 'fine-grained-assertions-test',
+  });
+  // THEN
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Lambda::Function', 1);
   //   template.hasResourceProperties('AWS::SQS::Queue', {
   //     VisibilityTimeout: 300
   //   });
