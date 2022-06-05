1fa1ec9c838a6b49bd07d0e74ef0736124f0da51
junkor-1011 <tomoyukizxy@gmail.com>
2022-06-05 18:38:10 +0900

    feat: linter & formatter

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'import', 'jest'],
+  rules: {
+    'import/extensions': [
+      2,
+      {
+        js: 'never',
+        ts: 'never',
+        json: 'always',
+      },
+    ],
+  },
+};
